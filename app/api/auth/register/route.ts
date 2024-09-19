4095754ccd50dd6d88645839d9530e0c8f12fb63
amjido-01 <youndsadeeq10@gmail.com>
2024-09-19 13:12:34 +0100

    commit

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,35 +1,40 @@
 import bcrypt from "bcrypt";
 import {NextResponse} from "next/server" ;
-import { prisma } from "@/lib/prismaDB"
+import { prisma } from "../../../../lib/prismaDB"
 
 export async function POST(request: any) {
-    const body = await request.json()
-
-    const {email, password, userName, name } =  body;
+  try {
+    const body = await request.json();
+    const { email, password, userName, name } = body;
 
     if (!email || !password || !userName || !name) {
-        return new NextResponse("Missing Fields", { status: 400 });
+      return new NextResponse("Missing Fields", { status: 400 });
+    }
+
+    const exist = await prisma.user.findUnique({
+      where: { email }
+    });
+
+    if (exist) {
+      return new NextResponse("Email already exists", { status: 400 });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        userName,
+        name,
+        password: hashedPassword
       }
+    } as any);
 
-      const exist = await prisma.user.findUnique({
-        where: {email}
-      })
-
-      if (exist) {
-        throw new Error("Email already exists");
-      }
-
-      const hashedPassword = await bcrypt.hash(password, 10);
-
-      const user = await prisma.user.create({
-        data: {
-            email,
-            userName,
-            name,
-            password: hashedPassword
-        }
-      } as any)
-      console.log(user, "user from register")
-      
-      return NextResponse.json(user);
-}   
+    console.log(user, "user from register");
+    
+    return NextResponse.json(user);
+  } catch (error) {
+    console.error("Error in registration:", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
